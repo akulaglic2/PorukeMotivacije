d32a7fe15e8bcf8f18e218e473fc629cf7a63586
Azra Ibric <azra.ibric@epti.com>
2022-02-14 22:50:28 +0100

    Created list of category - for test taken data from quotes

--- src/pages/Tree/index.js
+++ src/pages/Tree/index.js
@@ -5,6 +5,8 @@ import UpArrowIcon from "../Root/assets/up-arrow-icon.png";
 import * as style from "../../assets/common/Styles";
 
 import { Flex, styled } from "reakit";
+import { connect } from "react-redux";
+import { reduxForm } from "redux-form";
 
 const LogoIcon = styled.img`
   height: 20px;
@@ -32,8 +34,15 @@ const CategoryContent = styled.div`
   flex-direction: column;
 `;
 
+const HiddenStyled = styled(Hidden)`
+  color: white;
+  cursor: pointer;
+  margin: 13px 68px;
+  font-size: ${style.Fonts.FontSize.medium};
+`;
+
 const Tree = (props) => {
-  const { active } = props;
+  const { active, posts } = props;
   return (
     <Hidden.Container>
       {(hidden) => (
@@ -48,21 +57,25 @@ const Tree = (props) => {
               <Label active={active}>Category</Label>
             </Wrapper>
           </Hidden.Toggle>
-          <Hidden
-            style={{
-              color: "white",
-              cursor: "pointer",
-              margin: "13px 68px",
-              fontSize: "medium",
-            }}
-            {...hidden}
-          >
-            Hidden
-          </Hidden>
+
+          {posts.map((data, index) => (
+            <HiddenStyled {...hidden}>{data.title}</HiddenStyled>
+          ))}
         </CategoryContent>
       )}
     </Hidden.Container>
   );
 };
 
-export default Tree;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.quotes,
+  };
+};
+
+export default connect(mapStateToProps)(
+  reduxForm({
+    // a unique name for the form
+    form: "TreeForm",
+  })(Tree)
+);
